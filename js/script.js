c7404dc0f0c5dccb46bf37c439f13512c3d1ec7f
Pavel <example@gmail.com>
2022-07-18 18:29:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// alert("Hande hoch");
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Enter your name", "");
+// answers[1] = prompt("Enter your last name", "");
+// answers[2] = prompt("Enter your age", "");
+// document.write(answers);
+// console.log(answers);
+
+// // console.log(typeof(answers));
+// const category = 'toys';
+// // console.log('https://someurl.com/' + category + '/' + '4');
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Pavel";
+// alert(`Hello, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(9%6);
+
+console.log(2 + 2 * 2 != 8);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
+
+
