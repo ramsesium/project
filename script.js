e6bc5447a88ea252dd843c9e675dc9a5e91a4e04
Pavel <example@gmail.com>
2022-07-21 17:35:53 +0300

    lesson 15

--- script.js
+++ script.js
@@ -177,27 +177,72 @@
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-for ( let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done');
-    } else {
-        console.log('Error');
-        i--; // если условие не выполнилось мы возвращаемся на 1 итерацию назад и еще раз задаем вопросы пользователю
-    }
-}
-
-console.log(personalMovieDB);
+// 1 way
+// for ( let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+// 
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('Done');
+//     } else {
+//         console.log('Error');
+//         i--; // если условие не выполнилось мы возвращаемся на 1 итерацию назад и еще раз задаем вопросы пользователю
+//     }
+// }
+// 
+// 
+// 2 way
+// let num = 1;
+// 
+// while (num <= 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+//     num++;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('Done');
+//     } else {
+//         console.log('Error');
+//         num--;
+//     };
+// }
+// 
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// 
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+// 
+// // 3 way
+// let num = 0;
+// do { 
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+//     num++;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('Done');
+//     } else {
+//         console.log('Error');
+//         num--;
+//     };
+// }
+// 
+// while (num < 2);
+// 
+// if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+// console.log("Вы киноман");
+// } else {
+//     console.log("Error");
+// }
+// 
+// console.log(personalMovieDB);
