e5fcd08c6816ad48f84ce0e652706a9767811c72
Pavel <example@gmail.com>
2022-08-04 18:28:14 +0300

    lesson 23

--- script.js
+++ script.js
@@ -554,7 +554,7 @@ console.log(border);
 
 // LESSON 21----------------------------------------------
 
-const arr = [1, 32, 3, 16, 8, 10];
+// const arr = [1, 32, 3, 16, 8, 10];
 
 // сравнение идет посимвольное и отсортировался как строки
 // arr.sort();
@@ -655,5 +655,53 @@ const newNumbers = copy(numbers);
 newNumbers.a = 10;
 newNumbers.c.x = 10;
 
-console.log(newNumbers);
-console.log(numbers);
+// console.log(newNumbers);
+// console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+// console.log(Object.assign(numbers, add));
+const clone = Object.assign({}, add);
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();// клонирование массива(не по ссылке)
+
+newArray[1] = 'asdasdad';
+console.log(newArray);
+console.log(oldArray);
+
+//оператор разворота
+const video = ['youtube', 'vimeo', 'rutube'];
+const blogs = ['worldpress', 'livejournal', 'blogger'];
+const internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+const newARRAY = [...array];
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
+console.log(newObj);
+
+
