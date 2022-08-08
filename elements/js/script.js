d2e450bbad57c579d9013fc1b46682d2c813a42b
Pavel <example@gmail.com>
2022-08-08 16:14:43 +0300

    lesson 28

---
+++ elements/js/script.js
@@ -0,0 +1,25 @@
+//прежние методы получения элементов
+const box = document.getElementById('box');
+
+console.log(box);
+
+// const btns = document.getElementsByTagName('button');
+// const btns = document.getElementsByTagName('button')[1];
+const btns = document.getElementsByTagName('button');
+console.log(btns[0]);
+// console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+// современные методы
+
+const hearts = document.querySelectorAll('.heart'); // появляется метод for Each
+console.log(hearts);
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('div'); // позволяет получить только один элемент со страницы 
+// - тот который попался первым
+console.log(oneHeart);
