3a0a784480dd6f46ada2a2a9eabed30dbb56a61a
Pavel <example@gmail.com>
2022-08-08 15:02:53 +0300

    lesson 26

--- script.js
+++ script.js
@@ -867,3 +867,50 @@ personalMovieDB.writeYourGenres();
 // console.log(arrGenres);
 console.log(personalMovieDB);
 */
+
+// LESSON 26----------------------------------------------
+/*
+//1) через Стринг
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//2) сложение со строкой
+console.log(typeof(null + ''));
+
+const num = 5;
+console.log('https://vk.com/catalog' + num);
+
+const fontSize = 26 + 'px';
+
+// to number
+// 1)
+console.log(typeof(Number('4')));
+
+//2 унарный +
+console.log(typeof(+'4'));
+
+// 3 
+console.log(typeof(parseInt('15px', 10)));
+
+let answ = +prompt("Hello", "");
+
+
+// to boolean
+
+// 0, '', null, undefined, NaN; - --- false
+
+//1
+let switcher = null;
+switcher = 1;
+
+if (switcher) {
+    console.log('working...');
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3) !! преобразовывают в Булин
+console.log(typeof(!!"4444"));
+
+*/
