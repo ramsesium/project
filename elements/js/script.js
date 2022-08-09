8173ff2274ce854e36f98c8a3103c3cc175ebdd9
Pavel <example@gmail.com>
2022-08-09 18:04:13 +0300

    lesson 29end

--- elements/js/script.js
+++ elements/js/script.js
@@ -29,7 +29,59 @@ console.log(oneHeart);
 const box = document.getElementById('box');
 const btns = document.getElementsByTagName('button');
 const circles = document.getElementsByClassName('circle');
-const hearts = document.querySelectorAll('heart');
-const oneHeart = document.querySelector('.heart');
+const wrapper = document.querySelector('.wrapper');
+const hearts = wrapper.querySelectorAll('.heart');
+const oneHeart = wrapper.querySelector('.heart');
 
-console.dir(box); // показывает свойства объекта или прочего типа
+
+//console.dir(box); // показывает свойства объекта или прочего типа
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = 'background-color: green; width: 500px';
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я'); //старый метод
+//document.body.append(div); // мы берет тег боди и во внуть В КОНЕЦ его добавляем наш Див
+
+
+// ---------------------------------
+// Современные методы
+
+div.classList.add('black');
+
+wrapper.append(div);
+// wrapper.appendChild(div); // Устаревшие методы
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div);
+// hearts[0].after(div);
+
+// wrapper.insertBefore(div, hearts[1]); // Устаревшие методы
+
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]); // Устаревшие методы
+
+hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]); // Устаревшие методы
+
+div.innerHTML = "<h1>Hello world</h1>"; //1 metod // если хотим получить ХТМЛ
+// div.textContent = 'Help'; // если мы хотим получить только текст от пользователя
+
+// div.insertAdjacentHTML('beforebegin', '<h2>Privet</h2>');
+// div.insertAdjacentHTML('afterbegin', '<h2>Privet</h2>');
+// div.insertAdjacentHTML('beforeend', '<h2>Privet</h2>');
+// div.insertAdjacentHTML('afterend', '<h2>Privet</h2>');
