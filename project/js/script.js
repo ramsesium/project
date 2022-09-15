e233344df9ed68353bdff0040a4f78e6123c680e
Pavel <example@gmail.com>
2022-09-15 18:52:31 +0300

    lesson 33 begin

--- project/js/script.js
+++ project/js/script.js
@@ -120,6 +120,54 @@ movieDB.movies.forEach((film, i) => {
 
 5) Добавить нумерацию выведенных фильмов */
 
+
+// const adv = document.querySelectorAll('.promo__adv img'),
+//       poster = document.querySelector('.promo__bg'),
+//       genre = poster.querySelector('.promo__genre'),
+//       movieList = document.querySelector('.promo__interactive-list');
+
+// adv.forEach(item => {
+//     item.remove();
+// });
+
+// genre.textContent = "rjvtlbx";
+
+// // использовать разные кавычки для понимания движком
+// poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+// // 4
+// movieList.innerHTML = "";
+
+// movieDB.movies.sort();
+
+// movieDB.movies.forEach((film, i) => {
+//     movieList.innerHTML += `
+//     <li class="promo__interactive-item">${i + 1} ${film}
+//         <div class="delete"></div>
+//     </li>
+//     `;
+// });
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
 const movieDB = {
     movies: [
         "Логан",
@@ -169,31 +217,3 @@ movieDB.movies.forEach((film, i) => {
 
 
 
-
-
-// const adv = document.querySelectorAll('.promo__adv img'),
-//       poster = document.querySelector('.promo__bg'),
-//       genre = poster.querySelector('.promo__genre'),
-//       movieList = document.querySelector('.promo__interactive-list');
-
-// adv.forEach(item => {
-//     item.remove();
-// });
-
-// genre.textContent = "rjvtlbx";
-
-// // использовать разные кавычки для понимания движком
-// poster.style.backgroundImage = 'url("img/bg.jpg")';
-
-// // 4
-// movieList.innerHTML = "";
-
-// movieDB.movies.sort();
-
-// movieDB.movies.forEach((film, i) => {
-//     movieList.innerHTML += `
-//     <li class="promo__interactive-item">${i + 1} ${film}
-//         <div class="delete"></div>
-//     </li>
-//     `;
-// });
