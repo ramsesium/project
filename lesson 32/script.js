c9885a2155f1aff054ef323c1ca8e94f46de9b48
Pavel <example@gmail.com>
2022-09-15 18:45:41 +0300

    lesson 32

--- lesson 32/script.js
+++ lesson 32/script.js
@@ -21,3 +21,28 @@
 //     console.log(node);
 // }
 
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild); // get <div class="wrapper">
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode); // получаем <div class="first">
+// console.log(document.querySelector('#current').parentElement); //получает ELEMENT <div class="first">
+// console.log(document.querySelector('#current').parentNode.parentNode); // <div class="wrapper">
+
+// console.log(document.querySelector('[data-current="3"]').nextSibling); //получает узел переноса строки - он следует за data-current="3"
+
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling); //получает ELEMENT <li>4</li>
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+
+    console.log(node);
+} // ФОР ОФ перебрал БОДИ и вывел только элементы!!! не НОДЫ
+
