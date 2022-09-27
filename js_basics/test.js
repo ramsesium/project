1883dff849e5d25a63d0c88cebcdb07f0ac05eeb
Pavel <example@gmail.com>
2022-09-27 18:33:30 +0300

    Turkey

---
+++ js_basics/test.js
@@ -0,0 +1,13 @@
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (
+      userInput === "rock" ||
+      userInput === "paper" ||
+      userInput === "scissors"
+    ) {
+      return userInput;
+    } else {
+      console.log("Error");
+    }
+  };
+  console.log(getUserChoice("Paper"));
